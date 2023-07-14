38631e42847126def5c344446eed3cae476aeab0
Amy Clarke <125891006+AmyLeeClarke@users.noreply.github.com>
2023-07-14 15:46:38 +0100

    Add files via upload

--- SleepSQL.sql
+++ SleepSQL.sql
@@ -1,6 +1,11 @@
 USE sleep;
 SELECT * FROM health;
 
-SELECT sleep_duration, pa_level, COUNT(BMI) AS BMI_count FROM health 
-	WHERE sleep_duration < 7 
-    GROUP BY sleep_duration;
+SELECT COUNT(sleep_duration), COUNT(pa_level), COUNT(BMI) FROM health 
+	GROUP BY sleep_duration, pa_level, BMI;
+
+
+
+SELECT sleep_duration, COUNT(sleep_duration) FROM health 
+	GROUP BY sleep_duration 
+	ORDER BY COUNT(sleep_duration);
